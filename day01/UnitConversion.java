f520ae4d720291a435c680cb4dbc06aa991d4758
Furat <fmuntherre@wiley.com>
2026-06-06 00:19:30 +0300

    day01| static method usage  enahncement

--- day01/UnitConversion.java
+++ day01/UnitConversion.java
@@ -3,10 +3,9 @@ public class UnitConversion {
     public static void main(String[] args) {
         System.out.print("Hello!, it's Unit Converter Program\n");
 
-        Conversion conversion = new Conversion();
-        conversion.temperatureConverter();
-        conversion.weightConverter();
-        conversion.distanceConverter();
-        conversion.timeConverter();
+        Conversion.temperatureConverter();
+        Conversion.weightConverter();
+        Conversion.distanceConverter();
+        Conversion.timeConverter();
     }
 }
